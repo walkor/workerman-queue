184f419638e927e60216465ce77a8e1b3b25a6be
walkor <worker-man@qq.com>
2015-03-01 23:58:44 +0800

    add Timer

--- Applications/Queue/start.php
+++ Applications/Queue/start.php
@@ -41,15 +41,15 @@ $msg_recver->onWorkerStart = function($msg_recver)
 $msg_recver->onMessage = function($connection, $message) use ($msg_recver)
 {
     $msgtype = 1;
-    $errorcode = 0;
+    $errorcode = 500;
     // @see http://php.net/manual/zh/function.msg-send.php
-    if(msg_send( $msg_recver->queue , $msgtype , $message, true , true , $errorcode))
+    if(extension_loaded('sysvmsg') && msg_send( $msg_recver->queue , $msgtype , $message, true , true , $errorcode))
     {
         return $connection->send('{"code":0, "msg":"success"}');
     }
     else 
     {
-        return $connection->send('{"code":500, "msg":"'.$errorcode.'"}');
+        return $connection->send('{"code":'.$errorcode.', "msg":"fail"}');
     }
 };
 
@@ -58,55 +58,68 @@ $msg_recver->onMessage = function($connection, $message) use ($msg_recver)
 $consumer = new Worker();
 // 消费的队列的id
 $consumer->queueId = $QUEUE_ID;
+// 慢任务，消费者的进程数可以开多一些
+$consumer->count = 32;
 
 /**
  * 进程启动阻塞式的从队列中读取数据并处理
  */
 $consumer->onWorkerStart = function($consumer)
 {
-    if(extension_loaded('sysvmsg'))
-    {
-        // 获得队列资源
-        $consumer->queue = msg_get_queue($consumer->queueId);
-        while(1)
+    \Workerman\Lib\Timer::add(0.1, function() use ($consumer){
+        if(extension_loaded('sysvmsg'))
         {
-            $desiredmsgtype = 1;
-            $msgtype = 0;
-            $message = '';
-            $maxsize = 65535;
-            // 从队列中获取消息 @see http://php.net/manual/zh/function.msg-receive.php
-            msg_receive($consumer->queue , $desiredmsgtype , $msgtype , $maxsize , $message);
-             // 假设消息数据为json，格式类似{"class":"class_name", "method":"method_name", "args":[]}
-             $message = json_decode($message, true);
-             // 格式如果是正确的，则尝试执行对应的类方法
-             if(isset($message['class']) && isset($message['method']) && isset($message['args']))
-             {
-                 // 要调用的类名，加上Consumer命名空间
-                 $class_name = "\\Consumer\\".$message['class'];
-                 // 要调用的方法名
-                 $method = $message['method'];
-                 // 调用参数，是个数组
-                 $args = (array)$message['args'];
-                 
-                 // 类存在则尝试执行
-                 if(class_exists($class_name))
-                 {
-                     $class = new $class_name;
-                     $callback = array($class, $method);
-                     if(is_callable($callback))
-                     {
-                         call_user_func_array($callback, $args);
-                     }
-                     else
-                     {
-                         echo "$class_name::$method not exist\n";
-                     }
-                 }
-                 else
-                 {
-                     echo "$class_name not exist\n";
-                 }
-             }
-        }
-    }
+            // 获得队列资源
+            $consumer->queue = msg_get_queue($consumer->queueId);
+            // 循环取数据
+            while(1)
+            {
+                $desiredmsgtype = 1;
+                $msgtype = 0;
+                $message = '';
+                $maxsize = 65535;
+                // 从队列中获取消息 @see http://php.net/manual/zh/function.msg-receive.php
+                msg_receive($consumer->queue , $desiredmsgtype , $msgtype , $maxsize , $message, true, MSG_IPC_NOWAIT);
+                if(!$message)
+                {
+                    return;
+                }
+                // 假设消息数据为json，格式类似{"class":"class_name", "method":"method_name", "args":[]}
+                $message = json_decode($message, true);
+                // 格式如果是正确的，则尝试执行对应的类方法
+                if(isset($message['class']) && isset($message['method']) && isset($message['args']))
+                {
+                    // 要调用的类名，加上Consumer命名空间
+                    $class_name = "\\Consumer\\".$message['class'];
+                    // 要调用的方法名
+                    $method = $message['method'];
+                    // 调用参数，是个数组
+                    $args = (array)$message['args'];
+                     
+                    // 类存在则尝试执行
+                    if(class_exists($class_name))
+                    {
+                        $class = new $class_name;
+                        $callback = array($class, $method);
+                        if(is_callable($callback))
+                        {
+                            call_user_func_array($callback, $args);
+                        }
+                        else
+                        {
+                            echo "$class_name::$method not exist\n";
+                        }
+                        }
+                        else
+                        {
+                            echo "$class_name not exist\n";
+                        }
+                    }
+                    else 
+                    {
+                        echo "unknow message\n";
+                    }
+                }
+            }
+    });
 };
