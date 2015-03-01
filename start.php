1f7dd40a6a4fa443d5c5f8a032175953629da090
walkor <worker-man@qq.com>
2015-03-01 23:11:20 +0800

    start.php

---
+++ start.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * run with command 
+ * php start.php start
+ * or
+ * php start.php start -d
+ */
+
+ini_set('display_errors', 'on');
+use WorkerMan\Worker;
+
+require_once __DIR__ . '/Workerman/Autoloader.php';
+
+foreach(glob(__DIR__.'/Applications/*/start.php') as $start_file)
+{
+    require_once $start_file;
+}
+
+Worker::runAll();
